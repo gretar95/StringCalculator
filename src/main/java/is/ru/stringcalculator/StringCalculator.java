adc15692742e69e4b0d51712a5d3f5187a7e5a07
Gretar Gudmundsson <gretargudmundsson@clients-pool-1.nat.ru.is>
2017-10-13 13:15:42 +0000

    The calculator can now handle multiple numbers.

--- src/main/java/is/ru/stringcalculator/StringCalculator.java
+++ src/main/java/is/ru/stringcalculator/StringCalculator.java
@@ -9,12 +9,20 @@ public class StringCalculator{
 		else
 			if(text.contains(",")){
 				String[] numbers = text.split(",");
-				return toInt(numbers[0]) + toInt(numbers[1]);
+				return sum(numbers);
 			}
 			return 1;
 	}
 
 	private static int toInt(String number){
 		return Integer.parseInt(number);
+	}
+
+	private static int sum(String[] numbers){
+		int total = 0;
+		for(String number : numbers){
+			total += toInt(number);
+		}
+		return total;
 	}
 }
